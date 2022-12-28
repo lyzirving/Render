54f48f05a718358c728f076de87bec8e686c32d2
liuyuzhou <202002369@any3.com>
2022-12-28 13:32:34 +0800

    [ADD] copy assets to disk;
    Signed-off-by: liuyuzhou <202002369@any3.com>

--- app/src/main/java/com/lau/render/MainActivity.java
+++ app/src/main/java/com/lau/render/MainActivity.java
@@ -1,43 +1,105 @@
 package com.lau.render;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.PersistableBundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
 import com.lau.render.scene.Def;
 import com.lau.render.scene.Scene;
+import com.lau.render.utils.AssetsManager;
 import com.lau.render.utils.LogUtil;
+
+import java.util.Objects;
 
 /**
  * @author lyzirving
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-                                                               SurfaceHolder.Callback
+                                                               SurfaceHolder.Callback,
+                                                               AssetsManager.AssetsListener
 {
+    private static final int CODE_REQUEST_READ_WRITE_PERMISSION = 0x01;
     private static final String TAG = "MainActivity";
     private Scene mScene;
+    private SurfaceView mSurfaceView;
+    private MyHandler mHandler;
+
+    private class MyHandler extends Handler {
+
+        MyHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0: {
+                    mScene = new Scene(Def.MAIN);
+                    mSurfaceView.setVisibility(View.VISIBLE);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LogUtil.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-        mScene = new Scene(Def.MAIN);
 
-        SurfaceView surfaceView = findViewById(R.id.view_surface);
-        surfaceView.getHolder().addCallback(this);
+        mHandler = new MyHandler(Looper.getMainLooper());
+        AssetsManager.get().setListener(this);
+
+        mSurfaceView = findViewById(R.id.view_surface);
+        mSurfaceView.getHolder().addCallback(this);
+
+        requestUserPermission(CODE_REQUEST_READ_WRITE_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LogUtil.d(TAG, "onRequestPermissionsResult: code = " + requestCode);
+        switch (requestCode) {
+            case CODE_REQUEST_READ_WRITE_PERMISSION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    onPermissionGranted();
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         LogUtil.d(TAG, "onDestroy");
-        mScene.detachView();
-        mScene = null;
+        AssetsManager.get().destroy();
+        if(!Objects.isNull(mScene)) {
+            mScene.detachView();
+            mScene = null;
+        }
     }
 
     @Override
@@ -51,12 +113,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
         LogUtil.d(TAG, "surfaceChanged, width = " + width + ", height = " + height);
-        mScene.attachSurface(holder.getSurface());
+        if(!Objects.isNull(mScene)) {
+            mScene.attachSurface(holder.getSurface());
+        }
     }
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
         LogUtil.d(TAG, "surfaceDestroyed");
-        mScene.detachSurface();
+        if(!Objects.isNull(mScene)) {
+            mScene.detachSurface();
+        }
+    }
+
+    private void requestUserPermission(int type) {
+        switch (type) {
+            case CODE_REQUEST_READ_WRITE_PERMISSION: {
+                int permissionState = ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionState != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                                                      new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                                                      CODE_REQUEST_READ_WRITE_PERMISSION);
+                } else {
+                    onPermissionGranted();
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    private void onPermissionGranted() {
+        LogUtil.d(TAG, "onPermissionGranted");
+        AssetsManager.get().copyAssets(getApplicationContext());
+    }
+
+    @Override
+    public void onCopyFinish() {
+        mHandler.sendEmptyMessage(0);
+    }
+
+    @Override
+    public void onCopyFail() {
+
     }
 }
---
+++ app/src/main/java/com/lau/render/utils/AssetsManager.java
@@ -0,0 +1,215 @@
+package com.lau.render.utils;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author lyzirving
+ */
+public class AssetsManager {
+    private static final String TAG = "AssetsManager";
+    private static volatile AssetsManager sInstance;
+
+    private static final int END_OF_STREAM = -1;
+
+    public enum Type {
+        /**
+         * resources for rendering, such as .obj file
+         */
+        MODEL,
+        /**
+         * resources for rendering, such as .png, .jpg etc.
+         */
+        TEXTURES
+    }
+
+    private ThreadPoolExecutor mCachedThreadPool;
+    private AssetsListener mListener;
+
+    private AssetsManager() { buildThreadPoolIfNeed(); }
+
+    public static AssetsManager get() {
+        if (sInstance == null) {
+            synchronized (AssetsManager.class) {
+                if (sInstance == null)
+                    sInstance = new AssetsManager();
+            }
+        }
+        return sInstance;
+    }
+
+    private void buildThreadPoolIfNeed() {
+        if (mCachedThreadPool == null) {
+            mCachedThreadPool = new ThreadPoolExecutor(
+                    0, Integer.MAX_VALUE,
+                    60L, TimeUnit.SECONDS,
+                    new SynchronousQueue<Runnable>(),
+                    new AssetsThreadFactory(TAG),
+                    new ThreadPoolExecutor.AbortPolicy());
+        }
+    }
+
+    public void copyAssets(final Context ctx) {
+        buildThreadPoolIfNeed();
+        mCachedThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                File root = ctx.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                if (root != null) {
+                    boolean ret = true;
+                    if (!root.exists()) { ret = root.mkdirs(); }
+                    if (ret) {
+                        copyAssets(ctx, Type.MODEL, root.getAbsolutePath());
+                        copyAssets(ctx, Type.TEXTURES, root.getAbsolutePath());
+                        if (!Objects.isNull(mListener)) {
+                            mListener.onCopyFinish();
+                        }
+                    } else {
+                        LogUtil.e(TAG, "copyAssets: fail to make root directory[" + root.getAbsolutePath() + "]");
+                        if (!Objects.isNull(mListener)) {
+                            mListener.onCopyFail();
+                        }
+                    }
+                } else {
+                    LogUtil.e(TAG, "copyAssets: unknown err, fail to get external root");
+                    if (!Objects.isNull(mListener)) {
+                        mListener.onCopyFail();
+                    }
+                }
+            }
+        });
+    }
+
+    private void copyAssets(Context ctx, Type type, String dstRootDir) {
+        String typeStr = getAssetsTypeStr(type);
+        File dstTypeDir = new File(dstRootDir, typeStr);
+        if (!dstTypeDir.exists()) { dstTypeDir.mkdirs(); }
+        try {
+            String[] fileNames = ctx.getAssets().list(typeStr);
+            if (fileNames != null && fileNames.length > 0)
+                copyAssets(ctx, typeStr, fileNames, type, dstRootDir + "/" + typeStr);
+            else
+                LogUtil.e(TAG, "copyAssets: failed to list assets[" + typeStr + "]");
+        } catch (Exception e) {
+            LogUtil.d(TAG, "copyAssets: failed to get assets[" + typeStr + "], exception = " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void copyAssets(Context ctx, String assetsRoot, String[] assetsNames, Type type, String dstDirectory) {
+        for (String name : assetsNames) {
+            if (isAssetsFile(type, name)) {
+                copyFile(ctx, assetsRoot, name, dstDirectory);
+            } else if (name.contains(".")) {
+                LogUtil.e(TAG, "copyAssets: unknown type for name[" + assetsRoot + "/" + name + "], skip this");
+            } else {// directory
+                String path = assetsRoot + "/" + name;
+                String dstPath = dstDirectory + "/" + name;
+                File dstPathFile = new File(dstPath);
+                if (!dstPathFile.exists()) { dstPathFile.mkdirs(); }
+                try {
+                    String[] subFileNames = ctx.getAssets().list(path);
+                    if (subFileNames != null && subFileNames.length > 0)
+                        copyAssets(ctx, path, subFileNames, type, dstPath);
+                    else
+                        LogUtil.e(TAG, "copyAssets: failed to list assets in assets path[" + path + "]");
+                } catch (Exception e) {
+                    LogUtil.i(TAG, "copyAssets: failed to get assets in assets path[" + path + "], exception = " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void copyFile(Context ctx, String assetsRoot, String name, String dstDirectory) {
+        File dst = new File(dstDirectory, name);
+        if (!dst.exists()) {
+            String assetsFileDir = assetsRoot + "/" + name;
+            try {
+                InputStream is = ctx.getAssets().open(assetsFileDir);
+                FileOutputStream fos = new FileOutputStream(dst);
+                byte[] buffer = new byte[1024];
+                int byteCount;
+                while ((byteCount = is.read(buffer)) != END_OF_STREAM) {
+                    fos.write(buffer, 0, byteCount);
+                }
+                fos.flush();
+                is.close();
+                fos.close();
+            } catch (Exception e) {
+                LogUtil.e(TAG, "copyFile: exception happens for assets[" + assetsFileDir + "]"
+                             + ", dst[" + dst.getAbsolutePath() + "], message = " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void destroy() {
+        if (mCachedThreadPool != null) {
+            mCachedThreadPool.shutdown();
+            mCachedThreadPool = null;
+        }
+        mListener = null;
+    }
+
+    public void setListener(AssetsListener listener) {
+        mListener = listener;
+    }
+
+    private String getAssetsTypeStr(Type type) {
+        switch (type) {
+            case MODEL:
+                return "model";
+            case TEXTURES:
+                return "textures";
+            default:
+                return "";
+        }
+    }
+
+    private boolean isAssetsFile(Type type, String name) {
+        switch (type) {
+            case MODEL:
+                return name.contains(".obj") || name.contains(".png") || name.contains(".mtl")
+                        || name.contains(".blend") || name.contains(".txt");
+            case TEXTURES:
+                return name.contains(".png") || name.contains(".jpg");
+            default:
+                return false;
+        }
+    }
+
+    private static class AssetsThreadFactory implements ThreadFactory {
+        private final ThreadFactory mDefaultThreadFactory;
+        private final String mBaseName;
+        private final AtomicInteger mCount = new AtomicInteger(0);
+
+        public AssetsThreadFactory(final String baseName) {
+            mDefaultThreadFactory = Executors.defaultThreadFactory();
+            mBaseName = baseName;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            final Thread thread = mDefaultThreadFactory.newThread(r);
+            thread.setName(mBaseName + "-" + mCount.getAndIncrement());
+            return thread;
+        }
+    }
+
+    public static interface AssetsListener {
+        void onCopyFinish();
+        void onCopyFail();
+    }
+}
